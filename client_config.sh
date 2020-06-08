db422c9185681b139940d65507bd40c042e5f1e1
atticusstonestrom <atticusstonestrom@yahoo.com>
2020-06-08 14:09:16 -0400

    Update client_config.sh

--- client_config.sh
+++ client_config.sh
@@ -1,4 +1,5 @@
 ifconfig enp0s3 mtu 3000 up
+echo 0 > /proc/sys/net/ipv4/ip_forward
 iptables -A OUTPUT -o enp0s3 -t mangle -p tcp -m iprange ! --dst-range 192.168.43.0-192.168.43.255 -j MARK --set-mark 1
 echo 201 tunnel.out >> /etc/iproute2/rt_tables
 ip rule add fwmark 1 table tunnel.out
